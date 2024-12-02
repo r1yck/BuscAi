cab761304e45715c4487f85ec44e06c1d5358a1f
Henrique Fontoura <henriquefontoura.exe@gmail.com>
2024-12-01 22:52:02 -0300

    Alteração no buscai_db.sql

--- src/db/buscai_db.sql
+++ src/db/buscai_db.sql
@@ -4,21 +4,20 @@ CREATE DATABASE IF NOT EXISTS buscai_db;
 -- Seleciona o banco de dados
 USE buscai_db;
 
--- Criação da tabela Mercado
-CREATE TABLE mercado (
+-- Criação da tabela mercado
+CREATE TABLE IF NOT EXISTS mercado (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    localizacao VARCHAR(255) NOT NULL
+    nome VARCHAR(100) NOT NULL,
+    localizacao VARCHAR(100)
 );
 
--- Criação da tabela Produto com a relação com Mercado
-CREATE TABLE produto (
+-- Criação da tabela produto
+CREATE TABLE IF NOT EXISTS produto (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    preco DECIMAL(10, 2) NOT NULL,
-    disponivel BOOLEAN NOT NULL,
-    categoria VARCHAR(50) NOT NULL,   -- Categoria como uma string
-    mercado_id INT,                   -- Relacionamento com Mercado
-    FOREIGN KEY (mercado_id) REFERENCES mercado(id)  -- Chave estrangeira
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10, 2),
+    disponivel BOOLEAN,
+    categoria VARCHAR(50),
+    mercado_id INT,
+    FOREIGN KEY (mercado_id) REFERENCES mercado(id)
 );
-

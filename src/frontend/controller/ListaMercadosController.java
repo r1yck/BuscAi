a3ab799bf7fb05aef0027316469730257e059dc2
Mafe <mafxrnandxs@gmail.com>
2025-02-25 09:56:54 -0300

    front-end

--- src/frontend/controller/ListaMercadosController.java
+++ src/frontend/controller/ListaMercadosController.java
@@ -4,23 +4,34 @@
  */
 package frontend.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author mafxr
  */
-public class ListaMercadosController implements Initializable {
+public class ListaMercadosController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void voltaMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("frontend/views/MenuView.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    } 
     
 }
